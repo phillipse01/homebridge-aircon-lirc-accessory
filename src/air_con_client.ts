4858c2fc7d9d65e605b5b8ab98ac35a136826634
Edward Phillips <info@edfake.net>
2021-06-20 20:52:15 +1000

    general bugfix

--- src/air_con_client.ts
+++ src/air_con_client.ts
@@ -31,7 +31,7 @@ export default class AirConClient {
         let arrstr = str.split(".",1);
         let heat = arrstr[0] as unknown as number;
         let cool = arrstr[1] as unknown as number;
-        let mid = heat + cool / 2;
+        let mid = (heat + cool) / 2;
         logs.info("num "+mid);
         if (mid < 20.8) {
           atempVal = "Cold2"; }
